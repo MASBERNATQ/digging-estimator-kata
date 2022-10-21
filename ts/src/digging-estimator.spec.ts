545b98f0ee5d822b7e61666ed73092df5473dd7e
anebris <kenan.dufrene@live.fr>
2022-10-21 16:36:34 +0200

    should return InvalidFormatException or TunnelTooForDelayException

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,4 +1,4 @@
-import { DiggingEstimator } from "./digging-estimator";
+import { DiggingEstimator, InvalidFormatException, TunnelTooLongForDelayException } from "./digging-estimator";
 
 describe("digging estimator", () => {
   it("should return as Dr Pockovsky said", () => {
@@ -12,12 +12,22 @@ describe("digging estimator", () => {
     expect(result.total).toBe(48);
   });
 
-  /*it("should return an exception if day or length is not entier or if length or day is < 0", () => {
+  it("should return an InvalidFormatException", () => {
     // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
     const estimator = new DiggingEstimator();
+    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
+  
+    expect(() => estimator.tunnel(20.2, 2, "granite")).toThrowError(new InvalidFormatException());
+    expect(() => estimator.tunnel(20, 2.2, "granite")).toThrowError(new InvalidFormatException());
+    expect(() => estimator.tunnel(-1, 2, "granite")).toThrowError(new InvalidFormatException());
+    expect(() => estimator.tunnel(20, -1, "granite")).toThrowError(new InvalidFormatException());
+  });
 
-    const result = estimator.tunnel(28.2, 2, "granite");
+  it("should return an TunnelTooLongForDelayException", () => {
+    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
+    const estimator = new DiggingEstimator();
+    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
 
-    expect(result.total).toBe(48); // exception;
-  });*/
+    expect(() => estimator.tunnel(28, 1, "granite")).toThrowError(new TunnelTooLongForDelayException());
+  });
 });
