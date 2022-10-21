5a14a702c85692cf18eb062a31c9600e9936992b
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-10-21 17:23:52 +0200

    ✅ Add team composition

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,9 +1,9 @@
 import {
   DiggingEstimator,
   InvalidFormatException,
-  TunnelTooLongForDelayException,
+  Team,
   TeamComposition,
-  Team
+  TunnelTooLongForDelayException,
 } from "./digging-estimator";
 
 describe("DiggingEstimator", () => {
@@ -23,11 +23,6 @@ describe("DiggingEstimator", () => {
     expect(getDiggingRateForGranite).toHaveBeenCalledTimes(1);
   });
 
-  it("should return as Dr Pockovsky said", () => {
-    const teamComposition = estimator.tunnel(28, 2, "granite");
-    expect(teamComposition.total).toBe(48);
-  });
-
   it("should return an InvalidFormatException", () => {
     expect(() => estimator.tunnel(20.2, 2, "granite")).toThrow(new InvalidFormatException());
     expect(() => estimator.tunnel(20, 2.2, "granite")).toThrow(new InvalidFormatException());
@@ -39,32 +34,54 @@ describe("DiggingEstimator", () => {
     expect(() => estimator.tunnel(28, 1, "granite")).toThrow(new TunnelTooLongForDelayException());
   });
 
-  it("should return the composition of team for the day and the night", () => {
-    const teamComposition: TeamComposition = estimator.tunnel(15, 3, "granite");
-    const dayTeam: Team = teamComposition.dayTeam;
-    const nightTeam: Team = teamComposition.dayTeam;
-
-    // Day
-    expect(dayTeam.miners).toBe(2);
-    expect(dayTeam.healers).toBe(1);
-    expect(dayTeam.smithies).toBe(2);
-    expect(dayTeam.lighters).toBe(0);
-    expect(dayTeam.innKeepers).toBe(8);
-    expect(dayTeam.guards).toBe(0);
-    expect(dayTeam.guardManagers).toBe(0);
-    expect(dayTeam.washers).toBe(2);
-
-    // Night
-    expect(nightTeam.miners).toBe(2);
-    expect(nightTeam.healers).toBe(1);
-    expect(nightTeam.smithies).toBe(2);
-    expect(nightTeam.lighters).toBe(0);
-    expect(nightTeam.innKeepers).toBe(8);
-    expect(nightTeam.guards).toBe(0);
-    expect(nightTeam.guardManagers).toBe(0);
-    expect(nightTeam.washers).toBe(2);
+  it("should return team composition for 28m / 2 days / granite", () => {
+    const teamComposition: TeamComposition = estimator.tunnel(28, 2, "granite");
+    expect(teamComposition.total).toBe(48);
+    expect(teamComposition.dayTeam).toEqual({
+      miners: 3,
+      healers: 1,
+      smithies: 2,
+      lighters: 0,
+      innKeepers: 8,
+      guards: 0,
+      guardManagers: 0,
+      washers: 2,
+    });
+    expect(teamComposition.nightTeam).toEqual({
+      miners: 3,
+      healers: 1,
+      smithies: 2,
+      lighters: 4,
+      innKeepers: 12,
+      guards: 5,
+      guardManagers: 2,
+      washers: 3,
+    });
   });
 
-
+  it("should return team composition for 15m / 3 days / granite", () => {
+    const teamComposition: TeamComposition = estimator.tunnel(15, 3, "granite");
+    expect(teamComposition.total).toBe(15);
+    expect(teamComposition.dayTeam).toEqual({
+      miners: 2,
+      healers: 1,
+      smithies: 2,
+      lighters: 0,
+      innKeepers: 8,
+      guards: 0,
+      guardManagers: 0,
+      washers: 2,
+    });
+    expect(teamComposition.nightTeam).toEqual({
+      miners: 0,
+      healers: 0,
+      smithies: 0,
+      lighters: 0,
+      innKeepers: 0,
+      guards: 0,
+      guardManagers: 0,
+      washers: 0,
+    });
+  });
 });
 
