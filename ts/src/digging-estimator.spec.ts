6c389d414564c9bf6a1f692dc543858bb11b1f31
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-10-21 16:38:44 +0200

    🎨 Clean tests

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,33 +1,39 @@
-import { DiggingEstimator, InvalidFormatException, TunnelTooLongForDelayException } from "./digging-estimator";
+import {
+  DiggingEstimator,
+  InvalidFormatException,
+  TunnelTooLongForDelayException,
+} from "./digging-estimator";
 
-describe("digging estimator", () => {
+describe("DiggingEstimator", () => {
+  let estimator: DiggingEstimator;
+  let getDiggingRateForGranite: jest.SpyInstance<number[], [rockType: string]>;
+
+  beforeEach(() => {
+    estimator = new DiggingEstimator();
+    getDiggingRateForGranite = jest
+      .spyOn(estimator, "get")
+      .mockImplementation(() => [0, 3, 5.5, 7]);
+  });
+
+  it("should called get digging rate", () => {
+    estimator.tunnel(28, 2, "granite");
+    expect(getDiggingRateForGranite).toHaveBeenCalled();
+    expect(getDiggingRateForGranite).toHaveBeenCalledTimes(1);
+  });
+
   it("should return as Dr Pockovsky said", () => {
-    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
-
-    const estimator = new DiggingEstimator();
-    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
-  
-    const result = estimator.tunnel(28, 2, "granite");
-
-    expect(result.total).toBe(48);
+    const teamComposition = estimator.tunnel(28, 2, "granite");
+    expect(teamComposition.total).toBe(48);
   });
 
   it("should return an InvalidFormatException", () => {
-    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
-    const estimator = new DiggingEstimator();
-    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
-  
-    expect(() => estimator.tunnel(20.2, 2, "granite")).toThrowError(new InvalidFormatException());
-    expect(() => estimator.tunnel(20, 2.2, "granite")).toThrowError(new InvalidFormatException());
-    expect(() => estimator.tunnel(-1, 2, "granite")).toThrowError(new InvalidFormatException());
-    expect(() => estimator.tunnel(20, -1, "granite")).toThrowError(new InvalidFormatException());
+    expect(() => estimator.tunnel(20.2, 2, "granite")).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(20, 2.2, "granite")).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(-1, 2, "granite")).toThrow(new InvalidFormatException());
+    expect(() => estimator.tunnel(20, -1, "granite")).toThrow(new InvalidFormatException());
   });
 
   it("should return an TunnelTooLongForDelayException", () => {
-    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
-    const estimator = new DiggingEstimator();
-    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
-
-    expect(() => estimator.tunnel(28, 1, "granite")).toThrowError(new TunnelTooLongForDelayException());
+    expect(() => estimator.tunnel(28, 1, "granite")).toThrow(new TunnelTooLongForDelayException());
   });
 });
