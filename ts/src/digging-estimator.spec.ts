f4e293ce47bc838b32adf36471736c5da2fb8fc8
anebris <kenan.dufrene@live.fr>
2022-10-21 16:02:25 +0200

    mock get(rocktype) function

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,13 +1,23 @@
 import { DiggingEstimator } from "./digging-estimator";
 
 describe("digging estimator", () => {
-
   it("should return as Dr Pockovsky said", () => {
     // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
-    const estimator = new DiggingEstimator();
 
+    const estimator = new DiggingEstimator();
+    jest.spyOn(estimator, 'get').mockImplementation(() => [0, 3, 5.5, 7]);
+  
     const result = estimator.tunnel(28, 2, "granite");
 
     expect(result.total).toBe(48);
   });
+
+  /*it("should return an exception if day or length is not entier or if length or day is < 0", () => {
+    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
+    const estimator = new DiggingEstimator();
+
+    const result = estimator.tunnel(28.2, 2, "granite");
+
+    expect(result.total).toBe(48); // exception;
+  });*/
 });
