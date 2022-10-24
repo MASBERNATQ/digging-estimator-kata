589a27ebfe76582df8db834ec1a14942405280ae
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-10-24 22:08:51 +0200

    🎨 Move comments in example

--- ts/src/DiggingEstimator/digging-estimator.ts
+++ ts/src/DiggingEstimator/digging-estimator.ts
@@ -111,14 +111,15 @@ export class DiggingEstimator {
   /**
    * Get a list with number of meters dug per day and per team miners.
    *
+   * @example
+   * // For example, for granite it returns [0, 3, 5.5, 7]
+   * // if you put 0 dwarf, you dig 0m/d/team
+   * // if you put 1 dwarf, you dig 3m/d/team
+   * // 2 dwarves = 5.5m/d/team
+   * // so a day team on 2 miners and a night team of 1 miner dig 8.5m/d
    * @param {string} rockType Type of rock
    */
   private get(rockType: string): number[] {
-    // For example, for granite it returns [0, 3, 5.5, 7]
-    // if you put 0 dwarf, you dig 0m/d/team
-    // if you put 1 dwarf, you dig 3m/d/team
-    // 2 dwarves = 5.5m/d/team
-    // so a day team on 2 miners and a night team of 1 miner dig 8.5m/d
     const url = `dtp://research.vin.co/digging-rate/${rockType}`;
     console.log(`Tried to fetch ${url}`);
     throw new Error("Does not work in test mode");
