39a64354218acc9748a3362f20eaf37a98e2d2cd
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-10-26 21:32:47 +0200

    💬 Useless comment

--- ts/src/DiggingEstimator/digging-estimator.ts
+++ ts/src/DiggingEstimator/digging-estimator.ts
@@ -110,7 +110,6 @@ export class DiggingEstimator {
    * // For example, for granite it returns [0, 3, 5.5, 7]
    * // if you put 0 dwarf, you dig 0m/d/team
    * // if you put 1 dwarf, you dig 3m/d/team
-   * // 2 dwarves = 5.5m/d/team
    * // so a day team on 2 miners and a night team of 1 miner dig 8.5m/d
    * @param {string} rockType Type of rock
    */
