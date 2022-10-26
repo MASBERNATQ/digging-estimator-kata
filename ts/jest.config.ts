1a1a296a5f760ba27b3b7aba9a08b2ddfdb0951b
Quentin Masbernat <masbernat.quentin@gmail.com>
2022-10-26 21:47:05 +0200

    🎨 Clean jest config

--- ts/jest.config.ts
+++ ts/jest.config.ts
@@ -1,13 +1,10 @@
 export default {
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    collectCoverageFrom: [
-        "src/**/*.ts",
-        "!**/node_modules/**"
-    ],
-    rootDir: './',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    }
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
+  rootDir: "./",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
